170b519e348bb1f99844e7296253234c4a8c388a
rashadma <rashadkhm@code.edu.az>
2023-11-25 18:03:40 +0200

    Added table

---
+++ crypto-app/src/components/Table.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Table = () => {
+  return (
+    <div className="flex flex-col mt-9 border border-gray-100 rounded">
+      <table className="w-full table-auto">
+        <thead className="capitalize text-base text-gray-100 font-medium border-b border-gray-100">
+          <tr>
+            <th className="py-1">Asset</th>
+            <th className="py-1">Name</th>
+            <th className="py-1">Price</th>
+            <th className="py-1">Total Volume</th>
+            <th className="py-1">Market Cap Change</th>
+            <th className="py-1">1H</th>
+            <th className="py-1">24H</th>
+            <th className="py-1">7D</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="text-center text-base border-b border-gray-100 hover:bg-gray-200 last:border-b-0">
+            <td className="py-4">Asset</td>
+            <td className="py-4">Name</td>
+            <td className="py-4">Price</td>
+            <td className="py-4">Total Volume</td>
+            <td className="py-4">Market Cap Change</td>
+            <td className="py-4">1H</td>
+            <td className="py-4">24H</td>
+            <td className="py-4">7D</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
--- crypto-app/src/pages/Crypto.js
+++ crypto-app/src/pages/Crypto.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Table from "../components/Table";
 
 const Crypto = () => {
-  return <div>Crypto</div>;
+  return (
+    <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
+      <Table />
+    </section>
+  );
 };
 
 export default Crypto;
