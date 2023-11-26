eef0fb7d338c3608a7b229a3677d032c637bae9b
rashadma <rashadkhm@code.edu.az>
2023-11-26 14:57:24 +0200

    axios added and cryptos fetched

--- crypto-app/src/components/Table.js
+++ crypto-app/src/components/Table.js
@@ -2,10 +2,6 @@ import React, { useContext } from "react";
 import { CryptoContext } from "../context/CryptoContext";
 
 const Table = () => {
-  const { test } = useContext(CryptoContext);
-
-  console.log(test);
-
   return (
     <div className="flex flex-col mt-9 border border-gray-100 rounded">
       <table className="w-full table-auto">
--- crypto-app/src/context/CryptoContext.js
+++ crypto-app/src/context/CryptoContext.js
@@ -1,13 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
 export const CryptoContext = createContext({});
 
-export const CryptoProvider = ({children}) => {
-      const [test, setTest] = useState("Test")
+export const CryptoProvider = ({ children }) => {
+  const [data, setData] = useState();
 
-      return(
-            <CryptoContext.Provider value={{test, setTest}}>
-                  {children}
-            </CryptoContext.Provider>
-      )
-}
- 
+  const fetchCryptoDatas = async () => {
+    try {
+      let res = await axios.get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en"
+      );
+      if (res) {
+        console.log("fetched data", res);
+        setData(res.data);
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+  useEffect(() => {
+    fetchCryptoDatas();
+  }, []);
+
+  return <CryptoContext.Provider value={{}}>{children}</CryptoContext.Provider>;
+};
