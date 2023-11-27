0827daf4220afffdc62407b1a321e06edd834dd1
rashadma <rashadkhm@code.edu.az>
2023-11-27 17:34:17 +0200

    Added loading animation

--- crypto-app/src/components/Search.js
+++ crypto-app/src/components/Search.js
@@ -5,8 +5,9 @@ import debounce from "lodash.debounce";
 
 const SearchInput = ({ handleSearch }) => {
   const [search, setSearch] = useState("");
-  let { searchedData, setCoinSearch, setSearchedData } = useContext(CryptoContext);
-  
+  let { searchedData, setCoinSearch, setSearchedData } =
+    useContext(CryptoContext);
+
   let hangleChangeInput = (e) => {
     e.preventDefault();
     let query = e.target.value;
@@ -14,14 +15,23 @@ const SearchInput = ({ handleSearch }) => {
     handleSearch(query);
   };
 
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    handleSearch(search);
+  };
+
   const selectCoin = (item) => {
-    setCoinSearch(item)
-    setSearch('')
-    setSearchedData()
-  }
+    setCoinSearch(item);
+    setSearch("");
+    setSearchedData();
+  };
+
   return (
     <>
-      <form className="w-96 relative flex items-center ml-7 font-nunito">
+      <form
+        className="w-96 relative flex items-center ml-7 font-nunito"
+        onSubmit={handleSubmit}
+      >
         <input
           onChange={hangleChangeInput}
           value={search}
@@ -37,23 +47,31 @@ const SearchInput = ({ handleSearch }) => {
 
       {search.length > 0 ? (
         <ul className="absolute top-11 right-0 w-96 h-96 rounded overflow-x-hidden bg-gray-200 bg-opacity-60 backdrop-blur-md">
-          {
-            searchedData ? 
-            searchedData.map(item => {return <li className="flex items-center ml-4 my-2 cursor-pointer"
-            key={item.id}
-            onClick={() => selectCoin(item.id)}
-            >
-                 <img
-                      className="w-[1rem] h-[1rem] mx-1.5"
-                      src={item.thumb}
-                      alt={item.name}
-                    />
-                    <span>
-                        {item.name}
-                    </span>
-            </li>})
-             : <h2>Loading...</h2>
-          }
+          {searchedData ? (
+            searchedData.map((item) => {
+              return (
+                <li
+                  className="flex items-center ml-4 my-2 cursor-pointer"
+                  key={item.id}
+                  onClick={() => selectCoin(item.id)}
+                >
+                  <img
+                    className="w-[1rem] h-[1rem] mx-1.5"
+                    src={item.thumb}
+                    alt={item.name}
+                  />
+                  <span>{item.name}</span>
+                </li>
+              );
+            })
+          ) : (
+            <div className="w-full h-full flex justify-center items-center">
+              <div
+                className="w-8 h-8 border-4 border-cyan rounded-full border-b-gray-200 animate-spin"
+                role="status"
+              />
+            </div>
+          )}
         </ul>
       ) : null}
     </>
