ea6560f28c0a9f6c06a92e5d988593fb566fec64
rashadma <rashadkhm@code.edu.az>
2023-11-28 17:09:55 +0200

    Pagination progress

--- crypto-app/src/components/Pagination.js
+++ crypto-app/src/components/Pagination.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import paginationArrow from "../assets/pagination-arrow.svg";
+import { CryptoContext } from "../context/CryptoContext";
 
 const Pagination = () => {
+  let { page, setPage, totalPages } = useContext(CryptoContext);
+  const AllPage = Math.ceil(totalPages / 10);
+
+  const nextPage = () => {
+    if (page === AllPage) {
+      return null;
+    } else {
+      setPage(page + 1);
+    }
+  };
+  console.log();
+  const prevPage = () => {
+    if (page === AllPage) {
+      return null;
+    } else {
+      setPage(page - 1);
+    }
+  };
+
   return (
     <div className="flex items-center">
       <ul className="flex items-center justify-end text-sm">
         <li className="flex items-center">
-          <button className="outline-0 hover:text-cyan w-8">
+          <button onClick={prevPage} className="outline-0 hover:text-cyan w-8">
             <img
               className="w-full h-auto rotate-180"
               src={paginationArrow}
@@ -14,41 +34,58 @@ const Pagination = () => {
             />
           </button>
         </li>
-        <li>
-          <button className="outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center text-lg">
+        {/* <li>
+          <button className="outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center ">
             ...
           </button>
-        </li>
+        </li> */}
+
+        {page - 1 !== 0 ? (
+          <li>
+            <button
+              onClick={prevPage}
+              className="outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center bg-gray-200 mx-1.5"
+            >
+              {page - 1}
+            </button>
+          </li>
+        ) : null}
         <li>
-          <button className="outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center text-lg bg-gray-200 mx-1.5">
-            1
+          <button
+            disabled
+            className="outline-0 rounded-full w-8 h-8 flex items-center justify-center  bg-cyan text-gray-300 mx-1.5"
+          >
+            {page}
           </button>
         </li>
-        <li>
-          <button className="outline-0 rounded-full w-8 h-8 flex items-center justify-center text-lg bg-cyan text-gray-300 mx-1.5">
-            2
-          </button>
-        </li>
-        <li>
-          <button className="outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center text-lg bg-gray-200 mx-1.5">
-            3
-          </button>
-        </li>
-        <li>
-          <button className="outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center text-lg">
+        {page + 1 !== AllPage && page !== AllPage ? (
+          <li>
+            <button
+              onClick={nextPage}
+              className="outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center  bg-gray-200 mx-1.5"
+            >
+              {page + 1}
+            </button>
+          </li>
+        ) : null}
+        {/* <li>
+          <button className="outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center ">
             ...
           </button>
-        </li>
-        <li>
-          <button>Last page</button>
-        </li>
-        <li>
-          <button className="outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center text-lg bg-gray-200 mx-1.5">
-            100
-          </button>
-        </li>
+        </li> */}
+        <li>{/* <button>Last page</button> */}</li>
+        {page !== AllPage ? (
+          <li>
+            <button
+              onClick={() => setPage(AllPage)}
+              className="outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center bg-gray-200 mx-1.5"
+            >
+              {AllPage}
+            </button>
+          </li>
+        ) : null}
         <li className="flex items-center">
-          <button className="outline-0 hover:text-cyan w-8">
+          <button onClick={nextPage} className="outline-0 hover:text-cyan w-8">
             <img className="w-full h-auto" src={paginationArrow} alt="arrow" />
           </button>
         </li>
--- crypto-app/src/context/CryptoContext.js
+++ crypto-app/src/context/CryptoContext.js
@@ -8,11 +8,22 @@ export const CryptoProvider = ({ children }) => {
   const [coinSearch, setCoinSearch] = useState("");
   const [currency, setCurrency] = useState("Eur");
   const [sortBy, setSortBy] = useState("market_cap_desc");
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(250);
 
   const getCryptos = async () => {
     try {
+      const data = await fetch(`https://api.coingecko.com/api/v3/coins/list`)
+        .then((res) => res.json())
+        .then((json) => json);
+      setTotalPages(data.length);
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
       let res = await axios.get(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=10&page=${page}&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`
       );
       if (res) {
         setData(res.data);
@@ -25,19 +36,6 @@ export const CryptoProvider = ({ children }) => {
   };
 
   const getSearchedCryptos = async (query) => {
-    // try {
-    //   let res = await axios.get(
-    //     `https://api.coingecko.com/api/v3/search?query=${query}`
-    //   );
-    //   if (res) {
-    //     console.log("123", res);
-    //     setSearchedData(res.data.data.coins);
-    //   } else {
-    //     throw new Error();
-    //   }
-    // } catch (error) {
-    //   console.log("Error: ", error);
-    // }
     try {
       const data = await fetch(
         `https://api.coingecko.com/api/v3/search?query=${query}`
@@ -52,7 +50,7 @@ export const CryptoProvider = ({ children }) => {
 
   useLayoutEffect(() => {
     getCryptos();
-  }, [coinSearch, currency, sortBy]);
+  }, [coinSearch, currency, sortBy, page]);
 
   return (
     <CryptoContext.Provider
@@ -66,6 +64,9 @@ export const CryptoProvider = ({ children }) => {
         setCurrency,
         sortBy,
         setSortBy,
+        page,
+        setPage,
+        totalPages,
       }}
     >
       {children}
