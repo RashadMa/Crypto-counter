bb73c2a263adf8edaf3ace2485d7092555db04a0
rashadma <rashadkhm@code.edu.az>
2023-12-06 14:54:29 +0200

    Saved function is processed

---
+++ crypto-app/src/context/SavedContext.js
@@ -0,0 +1,40 @@
+import { createContext, useLayoutEffect, useState } from "react";
+export const SavedContext = createContext({});
+
+export const SavedProvider = ({ children }) => {
+  const [allCoins, setAllCoins] = useState([]);
+
+  const saveCoin = (coinId) => {
+    let oldCoins = JSON.parse(localStorage.getItem("coins"));
+
+    if (oldCoins.includes(coinId)) {
+      return null;
+    } else {
+      let newCoin = [...oldCoins, coinId];
+      setAllCoins(newCoin);
+      localStorage.setItem("coins", JSON.stringify(newCoin));
+    }
+  };
+
+  useLayoutEffect(() => {
+    let isThere = JSON.parse(localStorage.getItem("coins")) || false;
+
+    if (!isThere) {
+      localStorage.setItem("coins", JSON.stringify([]));
+    } else {
+      let totalCoins = JSON.parse(localStorage.getItem("coins"));
+      setAllCoins(totalCoins);
+    }
+  }, []);
+
+  return (
+    <SavedContext.Provider
+      value={{
+        saveCoin,
+        allCoins,
+      }}
+    >
+      {children}
+    </SavedContext.Provider>
+  );
+};
--- crypto-app/src/pages/Home.js
+++ crypto-app/src/pages/Home.js
@@ -4,17 +4,20 @@ import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 import { CryptoProvider } from "../context/CryptoContext";
 import { TrendingProvider } from "../context/TrendingContext";
+import { SavedProvider } from "../context/SavedContext";
 
 const Home = () => {
   return (
     <CryptoProvider>
       <TrendingProvider>
-        <main className="w-full flex flex-col first-letter:content-center items-center relative text-white font-nunito">
-          <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
-          <Logo />
-          <Navigation />
-          <Outlet />
-        </main>
+        <SavedProvider>
+          <main className="w-full flex flex-col first-letter:content-center items-center relative text-white font-nunito">
+            <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
+            <Logo />
+            <Navigation />
+            <Outlet />
+          </main>
+        </SavedProvider>
       </TrendingProvider>
     </CryptoProvider>
   );
